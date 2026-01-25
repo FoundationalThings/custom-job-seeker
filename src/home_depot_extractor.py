1498e6a2d5f683b4c108a53349ed4168347927c7
Donna J. Harris <donnajharris@gmail.com>
2026-01-25 00:48:45 -0500

    Refactor job extraction logic in home_depot_extractor
    Commented out the job extraction logic and added a placeholder job entry.

--- src/home_depot_extractor.py
+++ src/home_depot_extractor.py
@@ -12,24 +12,27 @@ def fetch_jobs_home_depot(url):
         # Wait for dynamic content to load
         page.wait_for_selector(".jobs-listing")  # Home Depot list
         
+        
         # Extract job details
         jobs = []
-        job_cards = page.locator("li")  # Home Depot job
-        for i in range(job_cards.count()):            
-            card = job_cards.nth(i)
-            all_info = card.locator("a.job-link").get_attribute("data-job")
-            title = all_info["title"]
-            link = all_info["url"]
+        # job_cards = page.locator("li")  # Home Depot job
+        # for i in range(job_cards.count()):            
+        #     card = job_cards.nth(i)
+        #     all_info = card.locator("a.job-link").get_attribute("data-job")
+        #     title = all_info["title"]
+        #     link = all_info["url"]
                                                                 
-            #title = card.locator(".job-title").inner_text()
-            #link = card.locator("a.job-link").get_attribute("href")
-            location = card.locator("a.job-location").inner_text()
+        #     #title = card.locator(".job-title").inner_text()
+        #     #link = card.locator("a.job-link").get_attribute("href")
+        #     location = card.locator("a.job-location").inner_text()
             
-            jobs.append({
-                "title": title,
-                "link": link,
-                "location": location
-            })
+        #     jobs.append({
+        #         "title": title,
+        #         "link": link,
+        #         "location": location
+        #     })
+
+        jobs.append({"title": page, "link": "N/A", "location": "N/A"})
         
         browser.close()
         return jobs
